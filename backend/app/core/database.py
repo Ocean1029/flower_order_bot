b93d6fac95d95089cc1521400be791f246ff5979
Ocean1029 <vanessa0201029@gmail.com>
2025-05-20 16:14:42 +0800

    update deployment setting

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -3,6 +3,8 @@ import os
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import declarative_base
 from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import AsyncGenerator
 
 load_dotenv()
 
@@ -16,6 +18,6 @@ engine = create_async_engine(DATABASE_URL, future=True, echo=False)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 Base = declarative_base()
 
-async def get_db() -> AsyncSession :
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSessionLocal() as session:
         yield session
